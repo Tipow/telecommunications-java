8160f737fd1ff4538c9feb12c623fd4c5c3cf79c
tiopw <habibamuhammed202@gmail.com>
2025-12-03 06:12:17 +0000

    tested the deskphone class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        DeskPhone myPhone = new DeskPhone("123456");
+        myPhone.callPhone("123456");
+        myPhone.answer();
     }
 }
